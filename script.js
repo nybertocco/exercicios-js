a1883b8ac44b52ef8717e97eb7928ba9a75c4483
Nycolas <nybertocco@gmail.com>
2024-02-19 12:54:07 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,81 @@
+/*Exercício 1
+alert("Hello World!")*/
+
+/*Exercício 2
+let n1 = 10
+let n2 = 5
+alert(n1 + n2)*/
+
+/*Exercício 3*/
+/*let type = 30
+
+if (typeof type === 'number') {
+    alert("É um número")
+}
+
+else {
+    alert("Não é um número")
+}*/
+
+/*Exercício 4*/
+/*let type = "cachorro"
+
+if (typeof type === 'string') {
+    alert("É uma string")
+}
+
+else {
+    alert("Não é uma string")
+}*/
+
+//Exercício 5
+/*let type = true
+
+if (typeof type === 'boolean') {
+    alert("É um boolean")
+}
+
+else {
+    alert("Não é um boolean")
+}*/
+
+//Exercício 6
+/*let n1 = 9
+let n2= 5
+alert(n1 -n2)*/
+
+//Exercício 7
+/*let n1 = 5
+let n2 = 5
+alert(n1 * n2)*/
+
+//Exercício 8
+/*let n1 = 10 
+let n2 = 2
+alert(n1 / n2)*/
+
+//Exercício 9
+/*let n1 = 8
+
+if (typeof n1 === 'number' && n1 % 2 === 0) {
+    alert("É um número par")
+}
+
+else {
+    alert("Não é um número par")
+}*/
+
+//Exercício 10
+/*let n1 = 5
+
+if (typeof n1 === 'number' && n1 % 2 === 1) {
+    alert("É um número ímpar")
+}
+
+else {
+    alert("Não é um número ímpar")
+}*/
+
+
+
+
